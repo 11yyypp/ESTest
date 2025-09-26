03f756121a4d5ecf344e6d80bd66eaf6c265dfcf
Patty <patty21479042@gmail.com>
2025-09-26 20:41:31 +0800

    2nd commit

---
+++ src/main/java/com/esun/test/ZcyApplication.java
@@ -0,0 +1,12 @@
+package com.esun.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZcyApplication.class, args);
+    }
+}
